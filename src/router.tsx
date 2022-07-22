e9190aebe92c80e46b6f6bb0cfcc627bd2ce942d
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-07-22 16:04:50 -0300

    add new routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,17 @@
+import CreateUser from 'pages/Create-User';
 import HomePage from 'pages/Homepage';
 import { Routes, Route } from 'react-router-dom';
 import { RoutePath } from 'types/routes';
 import Login from "./pages/Login/index";
+import Profile from "./pages/Profiles/index";
 
 const Router = () => {
     return (
         <Routes>
             <Route path={RoutePath.LOGIN} element={<Login/>} />
             <Route path={RoutePath.HOME} element={<HomePage/>} />
+            <Route path={RoutePath.CREATE_USER} element={<CreateUser/>} />
+            <Route path={RoutePath.PROFILE_SELECT} element={<Profile/>} />
         </Routes>
     );
 }
--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,6 +1,8 @@
 export enum RoutePath {
     LOGIN = '/',
     HOME = '/home',
+    CREATE_USER = '/create-user',
+    PROFILE_SELECT = '/profile/select',
     SETTINGS = '/settings',
     SETTINGS_GAMES = 'games',
     SETTINGS_TABLES = 'genres',
