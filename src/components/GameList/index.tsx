b1d783a787db6101f30c07eef8bb80a7e005948d
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-07-28 20:05:44 -0300

    feat(game):API connect (getAll) in home page

--- src/components/GameList/index.tsx
+++ src/components/GameList/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import GameCard from 'components/GameCard';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { findAllGames } from 'services/gameService';
 import * as S from "./style";
 
-const GameList = () => {
+ interface Game  {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  year: number;
+  image: string;
+  TrailerYouTubeUrl: string;
+  GameplayYouTubeUrl: string;
+  genreName: string;
+  ImdbScore: number;
+} 
+
+ const GameList = () => {
+
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => { 
+  getAllGames();
+  },[]);
+
+  const getAllGames = async () => { 
+    const response = await findAllGames.allGames()  ;
+    console.log(response);
+    setGames(response.data);
+  }
+
   return (
   <S.GamesSection>
-    <h1>Favorite Games</h1>
+    <h1>All Games</h1>
     <S.GameList>
-      <S.GameBox />
-      <S.GameBox />
-      <S.GameBox />
-      <S.GameBox />
+      {games.map((game, index)=>(
+      <GameCard game={game} key={index} />
+      ))}
     </S.GameList>
   </S.GamesSection>
   )
--- src/components/GameList/style.ts
+++ src/components/GameList/style.ts
@@ -3,25 +3,19 @@ import styled, { css } from "styled-components";
 
 export const GamesSection = styled.section`
 	${({ theme }) => css`
+	font-size: 3rem;
 	`}
 `;
 
 export const GameList = styled.section`
 	${({ theme }) => css`
     display: flex;
-flex-direction: row;
-flex-wrap: wrap;
-gap: 2rem;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 1rem;
+	margin-top: 1rem;
+	font-size: 1rem;
 	`}
 `;
 
 
-export const GameBox = styled.div`
-	${({ theme }) => css`
-	width:20.813rem;
-height: 18.75rem;
-left: 10.625px;
-top: 13.375px;
-background-color: gray;
-	`}
-`;
