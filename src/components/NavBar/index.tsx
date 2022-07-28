ff335f94e3f863a2f335e9ab36c4a3afb2b7ce59
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-07-28 20:06:34 -0300

    fix(navbar: css adjust and links

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import * as S from "./style";
 import gameMGM from 'assets/icons/game-add.svg'
-import userMGM from 'assets/icons/users.svg'
+import userMGM from 'assets/icons/user-settings.svg'
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+  let Navigate = useNavigate();
+  function goToGameSettings() {
+  Navigate("/game-settings");
+}
+
   return (
-    <S.NavBarIcons>
-        
-        <S.UserBox>
-          <h1>FOTO DO USUÁRIO (GETBYID)</h1>
-            <h1>NOME DO USUÁRIO OU PERFIL</h1>
-        </S.UserBox>
-        <S.NavBarIcons>
-        <S.GameManagementSVG src={gameMGM}/>
-        <S.UserManagementSVG  src={userMGM}/>
-        </S.NavBarIcons>
+    <S.NavBarIcons> 
+            <S.UserIcon/>
+            <h1>DANIEL</h1>
+          <S.GameManagementSVG src={gameMGM} onClick={goToGameSettings} />
+        <S.UserManagementSVG  src={userMGM} />
     </S.NavBarIcons>
   )
 }
--- src/components/NavBar/style.ts
+++ src/components/NavBar/style.ts
@@ -2,24 +2,41 @@ import styled, { css } from "styled-components";
 
 export const NavBarIcons = styled.section`
 	${({ theme }) => css`
+	display:flex;
+	flex-direction: row; 
+	gap: 1rem;
+	margin-left: 3rem;
+	padding: 2rem;
 	`}
 `;
 
-export const UserBox = styled.div`
-	${({ theme }) => css`
-	`}
+export const UserIcon = styled.img`
+  ${({ theme }) => css``}
+  cursor: pointer;
+  width: 3rem;
+  height: 3rem;
+  background-color: gray;
+  border-radius: 50%;
+
 `;
+
 
 export const  GameManagementSVG= styled.img`
 	${({ theme }) => css`
+	width: 3rem;
+	transition: all .3s ease-in-out;
+  :hover {
+    transform: scale(1.1);
+  }
 	`}
 `;
 
 export const UserManagementSVG = styled.img`
 	${({ theme }) => css`
-	`}
-`;
-export const UserIcons = styled.div`
-	${({ theme }) => css`
+	width: 3rem;
+	transition: all .3s ease-in-out;
+  :hover {
+    transform: scale(1.1);
+  }
 	`}
 `;
