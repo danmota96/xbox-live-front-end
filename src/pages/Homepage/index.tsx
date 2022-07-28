f464f878a7c31d617f925dac29aa032d1114bad2
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-07-28 20:35:51 -0300

    add(homepage): implement new features to homepage & css fix

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,19 +1,28 @@
 import GameList from 'components/GameList';
 import GenreList from 'components/GenreList';
 import NavBar from 'components/NavBar';
-import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { findAllGames } from 'services/gameService';
+import back from "assets/icons/back.svg";
 import * as S from "./style";
-import swall from 'sweetalert';
+
 
 const HomePage = () => {  
+  let Navigate = useNavigate();
+  function goToStart() {
+  Navigate("/profile/select");
+}
   return (
     <S.HomePage>
+      <S.BackButton >
+        <img src={back} onClick={goToStart} />
+        </S.BackButton>
       <NavBar />
+      <S.BoxItems>
       <GameList />
+      {/* <FavoriteList/> */}
       <GenreList /> 
-      
+      </S.BoxItems>
+
     </S.HomePage>
   )
 }
--- src/pages/Homepage/style.ts
+++ src/pages/Homepage/style.ts
@@ -1,16 +1,29 @@
 import styled, { css } from "styled-components";
+import background from "assets/imgs/background.png"
 
 export const HomePage = styled.section`
 	${({ theme }) => css`
-		background-color: ${theme.colors.baseBg1};
+		background-image: url(${background}); 
 		min-width: 100vw;
 		min-height: 100vh;
 		color: ${theme.colors.textColor};
 		font-family: 'Roboto';
-		/* background-image: url('./assets/imgs/background.png');
-		background-repeat: no-repeat;
-  		background-size: cover;
-  		background-position: center; */
+		
 	`}
 `;
 
+export const BoxItems = styled.section`
+	${({ theme }) => css`
+		margin: 2rem 0 0 5rem;
+	`}
+`;
+
+export const BackButton = styled.div`
+	${() => css`
+		cursor:pointer;
+		padding: 1rem;
+		img {
+		width:2rem;
+		}
+	`}
+`;
