ea91e00e92d085a4f80897323c1fcb3478d8b118
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-08-09 20:11:30 -0300

    index fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ThemeProvider } from 'styled-components';
-import theme from 'assets/styles/theme';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
-
-
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-     
-          <Router />
-       
-      </BrowserRouter>
-    </ThemeProvider>
+    <Router/>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from 'assets/styles/theme';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +12,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </ThemeProvider>
+   
   </React.StrictMode>
 );
 
