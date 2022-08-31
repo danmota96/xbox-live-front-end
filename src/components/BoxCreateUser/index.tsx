3c5d39e9f61187f2906361fbff69e7a6a17acf7f
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-08-30 21:08:28 -0300

    Update index.tsx

--- src/components/BoxCreateUser/index.tsx
+++ src/components/BoxCreateUser/index.tsx
@@ -1,9 +1,9 @@
-import ButtonLarge from 'components/ButtonLarge';
-import { SyntheticEvent, useState } from 'react';
-import { userService } from 'services/userService';
-import { useNavigate } from 'react-router-dom';
-import swall from 'sweetalert';
-import React from 'react';
+import ButtonLarge from "components/ButtonLarge";
+import { SyntheticEvent, useState } from "react";
+import { userService } from "services/userService";
+import { useNavigate } from "react-router-dom";
+import swall from "sweetalert";
+import React from "react";
 import * as S from "./style";
 
 interface userObj {
@@ -15,87 +15,59 @@ interface userObj {
   image: string;
 }
 const BoxCreateUser = () => {
+  let navigate = useNavigate();
 
-  let navigate = useNavigate(); 
-
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>){ 
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const newUser: userObj = { 
-    name: event.currentTarget.nome.value,
-    email: event.currentTarget.email.value,
-    password: event.currentTarget.password.value,
-    confirmPassword: event.currentTarget.confirmPassword.value,
-    cpf: event.currentTarget.cpf.value,
-    image: event.currentTarget.image.value,
+    const newUser: userObj = {
+      name: event.currentTarget.nome.value,
+      email: event.currentTarget.email.value,
+      password: event.currentTarget.password.value,
+      confirmPassword: event.currentTarget.confirmPassword.value,
+      cpf: event.currentTarget.cpf.value,
+      image: event.currentTarget.image.value,
     };
     console.log(newUser);
-    
-    
+
     const req = await userService.postUser(newUser);
-    if (req?.status === 201) { 
-    navigate('/')
-    }  swall({
-      title: 'User created!',
-      icon: 'success',
+    if (req?.status === 201) {
+      navigate("/");
+    }
+    swall({
+      title: "User created!",
+      icon: "success",
       timer: 3000,
-    })
+    });
   }
 
-
   return (
-    <S.BoxCreateUser >
-         <S.BoxCreateUserText>
+    <S.BoxCreateUser>
+      <S.BoxCreateUserText>
         <span>Create user</span>
       </S.BoxCreateUserText>
-    <S.BoxCreateForm onSubmit={handleSubmit}>
-      <input 
-      type="text" 
-      placeholder="Name" 
-      name='nome' 
-      id='name'
-      
-      />
-      <input 
-      type="email" 
-      placeholder="Email"  
-      name='email' 
-      id='email' 
-    
-      />
-      <input 
-      type="password" 
-      placeholder="Password"  
-      name='password'
-      id='password' 
-     
-      />
-      <input 
-      type="password" 
-      placeholder="Confirm your password"  
-      name='confirmPassword' 
-      id='confirmPassword' 
-     
-      />
-      <input 
-      type="text" 
-      placeholder="cpf"  
-      name='cpf' 
-      id='cpf' 
-     
-      />
-      <input 
-      type="text" 
-      placeholder="image url"  
-      name='image' 
-      id='image' 
-   
-      />
-      
-      <ButtonLarge value={'Register'}/>
-    </S.BoxCreateForm>
+      <S.BoxCreateForm onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" name="nome" id="name" />
+        <input type="email" placeholder="Email" name="email" id="email" />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          id="password"
+        />
+        <input
+          type="password"
+          placeholder="Confirm your password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
+        <input type="text" placeholder="cpf" name="cpf" id="cpf" />
+        <input type="text" placeholder="image url" name="image" id="image" />
+
+        <ButtonLarge value={"Register"} />
+      </S.BoxCreateForm>
     </S.BoxCreateUser>
-  )
-}
+  );
+};
 
 export default BoxCreateUser;
